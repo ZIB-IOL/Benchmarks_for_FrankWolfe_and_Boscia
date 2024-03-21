0439466d762918b82e438331f7248ebda85b6e9a
Dominik Kuzinowicz <kuzinowicz@zib.de>
2024-03-21 19:02:24 +0100

    fixed variable typo

--- BenchmarksFWnB/src/FrankWolfe/objectives/build_FW_obj.jl
+++ BenchmarksFWnB/src/FrankWolfe/objectives/build_FW_obj.jl
@@ -15,12 +15,10 @@ function build_random(; n=100, k=30, seed=1234)
     
     A = Random.randn(rng, n, k)
     b = Random.randn(rng, n)
-    
-    m = 5*dim
 
-    A_sq = 2/m * transpose(A) * A
-    A_b = 2/m * transpose(A) * b
-    b_squared = 2/m * transpose(b) * b
+    A_sq = 2/n * transpose(A) * A
+    A_b = 2/n * transpose(A) * b
+    b_squared = 2/n * transpose(b) * b
 
     f(x) = (1/2) * (transpose(x) * A_sq * x) + (transpose(x) * A_b) + (1/2) * b_squared
     grad!(storage, x) = storage .= A_sq * x + A_b
