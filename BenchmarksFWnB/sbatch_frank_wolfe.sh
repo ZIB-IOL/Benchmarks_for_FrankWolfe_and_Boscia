9ef5aa8e63d162b48a5170f9598064b7159d5f73
Dominik Kuzinowicz <kuzinowicz@zib.de>
2024-06-04 17:25:28 +0200

    adjusted setups for lower dimension spread and increased available time to 2 hrs

--- BenchmarksFWnB/sbatch_frank_wolfe.sh
+++ BenchmarksFWnB/sbatch_frank_wolfe.sh
@@ -14,7 +14,7 @@
 # force-stopped by the server. If you make the expected time too long, it will
 # take longer for the job to start. Here, we say the job will take 5 minutes.
 #              d-hh:mm:ss
-#SBATCH --time=0-02:00:00
+#SBATCH --time=0-03:00:00
 
 # Define the partition on which the job shall run. May be omitted.
 # How much memory you need.
